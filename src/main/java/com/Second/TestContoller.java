a0f3dcdf1312a8a209fc5ad20cc6665f288ac6f5
Sumit Kumar Yadav <sumit14kumaryadav@gmail.com>
2022-08-10 12:28:37 +0530

    First Commit

---
+++ src/main/java/com/Second/TestContoller.java
@@ -0,0 +1,14 @@
+package com.Second;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestContoller {
+	@RequestMapping("/test")
+	@ResponseBody
+	public String handle() {
+		return "Hello World....";
+	}
+}
